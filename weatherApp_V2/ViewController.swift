7dbdc94055bc06ca95d87e0767ba9e801927048c
Omari-hajar <89646158+Omari-hajar@users.noreply.github.com>
2021-12-26 14:22:02 +0300

    Initial Commit

---
+++ weatherApp_V2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherApp_V2
+//
+//  Created by Hajar Alomari on 26/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
